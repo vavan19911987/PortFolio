a0131c030f1afedcb88659dffea3d21cb524150d
vavan19911987 <v.pulin@yahoo.com>
2021-06-03 19:24:01 +0200

    two commit подготовил шаблон для Portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -44,32 +44,42 @@ gulp.task('html', function () {
 });
 gulp.task('scripts', function () {
     return gulp.src("src/js/**/*.js")
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream());
+
 
 });
 
 gulp.task('fonts', function () {
     return gulp.src("src/fonts/**/*")
-        .pipe(gulp.dest('dist/fonts'));
+        .pipe(gulp.dest('dist/fonts'))
+        .pipe(browserSync.stream());
+
 
 });
 
 gulp.task('icons', function () {
     return gulp.src("src/icons/**/*")
-        .pipe(gulp.dest('dist/icons'));
+        .pipe(gulp.dest('dist/icons'))
+        .pipe(browserSync.stream());
+
 
 });
 
 gulp.task('mailer', function () {
     return gulp.src("src/mailer/**/*")
-        .pipe(gulp.dest('dist/mailer'));
+        .pipe(gulp.dest('dist/mailer'))
+        .pipe(browserSync.stream());
 
 });
 
 gulp.task('images', function () {
     return gulp.src("src/img/**/*")
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'));
+        .pipe(gulp.dest('dist/img'))
+        .pipe(browserSync.stream());
+    
+
 
 });
 
