a3b5ff3c3590d06473d739e99d2e2eba5d8f34c9
vavan19911987 <v.pulin@yahoo.com>
2021-08-13 12:42:09 +0200

    сделал блок проценты и блок что использую

--- dist/js/script.js
+++ dist/js/script.js
@@ -102,8 +102,15 @@ const whatsapp = document.querySelector('.icon-whatsapp');
         });
     }
 
+    const counters = document.querySelectorAll('.skills__ratings-counter'),
+          lines = document.querySelectorAll('.skills__ratings-line span');
+
+    counters.forEach((item, i) => {
+        lines[i].style.width = item.innerHTML;
+    });
 
 
+new WOW().init();
 
 });
 
